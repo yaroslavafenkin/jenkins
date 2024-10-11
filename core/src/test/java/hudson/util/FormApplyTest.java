4664a031bef2e0e77293fb8dfe721266642c391b
Yaroslav Afenkin <91559310+yaroslavafenkin@users.noreply.github.com>
2024-10-11 20:56:29 +0300

    Fail a test

---
+++ core/src/test/java/hudson/util/FormApplyTest.java
@@ -0,0 +1,13 @@
+package hudson.util;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FormApplyTest {
+
+    @Test
+    public void testFail() {
+        fail();
+    }
+}
